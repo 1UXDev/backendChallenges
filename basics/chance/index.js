7b0dce885355e00427048136e4c22e512fb402e2
mntzd <julien.leske@gmail.com>
2023-06-05 12:12:16 +0200

    challenges solved

--- basics/chance/index.js
+++ basics/chance/index.js
@@ -1,3 +1,6 @@
-// import { server } from "./server.js";
+import { server, chance } from "./server.js";
+const PORT = 8000;
 
-// server.listen(…
+server.listen(PORT, () => {
+  `Hello, my name is ${chance.name} and I am ${chance.age} years old. I am a ${chance.profession}.`;
+});
